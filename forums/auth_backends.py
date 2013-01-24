41a99b592889ae6e1841f669cd284d255dc9171e
Brian Rosner <brosner@gmail.com>
2013-01-23 23:58:31 -0700

    Added authenticate method to PermissionBackend
    Authentication backends require an authenticate method, but since we only
    implement permissions in our PermissionBackend we need to no-op it.

--- forums/auth_backends.py
+++ forums/auth_backends.py
@@ -3,6 +3,10 @@ class PermissionBackend(object):
     supports_object_permissions = True
     supports_anonymous_user = True
     
+    def authenticate(self, **kwargs):
+        # always return a None user
+        return None
+    
     def has_perm(self, user, perm, obj=None):
         if user.is_anonymous():
             return False
