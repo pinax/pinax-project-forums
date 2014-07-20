e7cf5d86c02a84f5227527a5ec857ec12fac828e
Patrick Altman <paltman@gmail.com>
2014-07-19 21:39:36 -0400

    Lint error

--- theforum/profiles/urls.py
+++ theforum/profiles/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, url
 from .views import ProfileEditView
 
 
-urlpatterns = patterns("",
+urlpatterns = patterns(
+    "",
     url(r"^profile/edit/", ProfileEditView.as_view(), name="profiles_edit")
 )
